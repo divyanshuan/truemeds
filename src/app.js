ce6f5940ed99e08d66a5bbcb7198f56cf128e5f5
divyanshu verma (202081) <89985317+divyanshuan@users.noreply.github.com>
2022-07-18 20:31:28 +0530

    added changes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import Navbar from "./components/navbar";
 
-import Doctors from './components/doctors/Doctors';
+import Doctors from "./components/doctors/Doctors";
+import Medicine from "./components/medicine/index.js";
 
 const App = () => {
   return (
     <div>
-       <Doctors/> 
+      {/* <Doctors /> */}
+      <Medicine />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
